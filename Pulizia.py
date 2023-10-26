ddf326d0edad8c4090c9991fba77a4c1f7982897
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2023-10-26 19:07:08 +0200

    Aggiornamenti, funzione generica per eliminare colonne di un dataset ,creazione di un notebook

--- Pulizia.py
+++ Pulizia.py
@@ -5,25 +5,33 @@ import pandas as pd
 
 castelli_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\castelli_csv_rsd.csv"
 fortezze_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\fortifortezze_csv_rsd.csv"
-strutture_ricettive = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\mappa-delle-strutture-ricettive.csv"
+strutture_ricettive_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\mappa-delle-strutture-ricettive.csv"
+torri_costiere_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\torri-costiere_csv_rsd.csv"
+musei_parchi_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\musei-gallerie-siti-archeologici_csv_rsd.csv"
 #castelli = pd.read_csv(castelli_csv,sep = ";")
 #print(castelli.head(10))
 
-def elimina_righe_c_f(dataframe):
-    df = pd.read_csv(dataframe,sep = ";")
-    df.drop(df.columns[[0,1]],axis =1, inplace= True)
-    df.drop(columns = ['data_caricamento'],inplace = True)
+def elimina_colonne(lista,dataframe):
+    df = pd.read_csv(dataframe, sep=";")
+    df.drop(columns=lista,inplace=True)
     return df
 
-
-castelli = elimina_righe_c_f(castelli_csv)
+castelli_eliminare = ["Id","TIPO","NOTE_1","OBJECTID_1","OBJECTID","data_caricamento"]
+#castelli = elimina_righe_c_f(castelli_csv)
+castelli = elimina_colonne(castelli_eliminare,castelli_csv)
 castelli.rename(columns={'SOTTO_TIPO':'Tipologia'},inplace= True)
 castelli['Tipologia'] = "castello";
 
-fortezze = elimina_righe_c_f(fortezze_csv)
-fortezze.drop(columns = ["OBJECTID_1"],inplace = True)
+fortezze_eliminare = ["OBJECTID","Id","OBJECTID_1","data_caricamento"]
+fortezze = elimina_colonne(fortezze_eliminare,fortezze_csv)
 
+torri_eliminare = ["ID","OBJECTID","RIFMAZ","NUM","OBJECTID_12","OBJECTID_1","data_caricamento"]
+torri_costiere = elimina_colonne(torri_eliminare,torri_costiere_csv)
+
+
+print(castelli.head(10))
 print(fortezze.head(10))
+print(torri_costiere.head(10))
 
 
 
