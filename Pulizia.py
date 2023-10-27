d02a7f8327eefe3002a9a4532896e116df4f3358
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2023-10-28 01:00:29 +0200

    Aggiornamenti, aggiunzione del dataset pulito,elimiazione dataset inutili

--- Pulizia.py
+++ Pulizia.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import pyproj
 #Definizione di una funzione che prendendo a parametro un file csv pulisce i file castelli
 #e forti e fortezze dato che hanno caratterstiche simili
 
@@ -8,6 +8,8 @@ fortezze_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\datas
 strutture_ricettive_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\mappa-delle-strutture-ricettive.csv"
 torri_costiere_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\torri-costiere_csv_rsd.csv"
 musei_parchi_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\musei-gallerie-siti-archeologici_csv_rsd.csv"
+castelli_prova = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_originali\\castelli.csv"
+
 #castelli = pd.read_csv(castelli_csv,sep = ";")
 #print(castelli.head(10))
 
@@ -16,7 +18,7 @@ def elimina_colonne(lista,dataframe):
     df.drop(columns=lista,inplace=True)
     return df
 
-castelli_eliminare = ["Id","TIPO","NOTE_1","OBJECTID_1","OBJECTID","data_caricamento"]
+castelli_eliminare = ["Id","TIPO","OBJECTID_1","OBJECTID","data_caricamento"]
 #castelli = elimina_righe_c_f(castelli_csv)
 castelli = elimina_colonne(castelli_eliminare,castelli_csv)
 castelli.rename(columns={'SOTTO_TIPO':'Tipologia'},inplace= True)
@@ -27,6 +29,10 @@ fortezze = elimina_colonne(fortezze_eliminare,fortezze_csv)
 
 torri_eliminare = ["ID","OBJECTID","RIFMAZ","NUM","OBJECTID_12","OBJECTID_1","data_caricamento"]
 torri_costiere = elimina_colonne(torri_eliminare,torri_costiere_csv)
+
+castelli_prova = elimina_colonne(castelli_eliminare,castelli_csv)
+print(castelli_prova.head(10))
+
 
 
 print(castelli.head(10))
