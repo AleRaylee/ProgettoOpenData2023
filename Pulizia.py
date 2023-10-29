5946fec78ea99ba82eeb30ea7ea11610d7002e8a
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2023-10-29 12:38:46 +0100

    Aggiornamenti, aggiunzione del dataset pulito,elimiazione dataset inutili

--- Pulizia.py
+++ Pulizia.py
@@ -14,19 +14,19 @@ castelli_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\datas
 #print(castelli.head(10))
 
 def elimina_colonne(lista,dataframe):
-    df = pd.read_csv(dataframe, sep=";")
+    df = pd.read_csv(dataframe, sep=",")
     df.drop(columns=lista,inplace=True)
     return df
 
 castelli_eliminare = ["Id","TIPO","OBJECTID_1","OBJECTID","cons","data_caricamento"]
 
-
+'''
 fortezze_eliminare = ["OBJECTID","Id","OBJECTID_1","data_caricamento"]
 fortezze = elimina_colonne(fortezze_eliminare,fortezze_csv)
 
 torri_eliminare = ["ID","OBJECTID","RIFMAZ","NUM","OBJECTID_12","OBJECTID_1","data_caricamento"]
 torri_costiere = elimina_colonne(torri_eliminare,torri_costiere_csv)
-
+'''
 castelli = elimina_colonne(castelli_eliminare,castelli_csv)
 castelli.rename(columns={'SOTTO_TIPO':'Tipologia'},inplace= True)
 castelli.rename(columns={'DENOMINAZI':'Denominazione'},inplace= True)
