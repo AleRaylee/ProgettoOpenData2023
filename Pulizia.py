df34a02e4f50c979ab710071a94611a16d223df6
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2023-11-04 16:28:54 +0100

    Aggiornamenti,Aggiungimento dell'ontologia il 3 è quella finale

--- Pulizia.py
+++ Pulizia.py
@@ -4,7 +4,7 @@ fortezze_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\datas
 strutture_ricettive_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset\\mappa-delle-strutture-ricettive.csv"
 torri_costiere_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset\\torri-costiere.csv"
 castelli_csv = "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset\\castelli.csv"
-
+comuni_csv= "C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset\\Elenco-comuni-italiani.csv"
 #castelli = pd.read_csv(castelli_csv,sep = ";")
 #print(castelli.head(10))
 def capitalize_column_name(column_name):
@@ -16,6 +16,8 @@ def elimina_colonne(lista,dataframe):
     df = pd.read_csv(dataframe, sep=",")
     df.drop(columns=lista,inplace=True)
     return df
+
+
 
 castelli_eliminare = ["Id","TIPO","SOTTO_TIPO","OBJECTID_1","OBJECTID","cons","data_caricamento"]
 
@@ -38,19 +40,35 @@ lista_colonne_t = torri_costiere.columns.tolist()
 castelli = castelli.rename(columns={col: capitalize_column_name(col) for col in lista_colonne_c})
 torri_costiere = torri_costiere.rename(columns={col: capitalize_column_name(col) for col in lista_colonne_t})
 
+strutture_ricettive = pd.read_csv(strutture_ricettive_csv,sep=';',encoding='ISO-8859-1')
+strutture_ricettive = strutture_ricettive.drop(['Regione'],axis=1)
 
 
+comuni = pd.read_csv(comuni_csv,sep =",")
+print(comuni.columns)
+#Prendo solo i comuni appartenenti alla regione Sicilia che ha codice regione 19
+comuni = comuni[comuni['Codice Regione'] == 19]
 
+# Seleziona solo le colonne desiderate
+columns_to_keep = ['Denominazione in italiano', 'Sigla automobilistica', 'Codice Comune formato numerico']
+comuni = comuni[columns_to_keep]
+comuni.rename(columns={'Denominazione in italiano':'Comune'},inplace= True)
+comuni.rename(columns={'Sigla automobilistica':'Provincia'},inplace= True)
+comuni.rename(columns={'Codice Comune formato numerico':'Codice_Istat'},inplace= True)
 
-print(castelli.head(10))
-print(castelli.columns)
-
+fortezze = fortezze.merge(comuni, left_on='PRO_COM_T', right_on='Codice_Istat', how='left')
+fortezze = fortezze.drop(['Codice_Istat'],axis=1)
 '''
 print(castelli.head(10))
 print(fortezze.head(10))
 print(torri_costiere.head(10))
+print(strutture_ricettive.head(10))
+print(castelli.head(10))
+print(castelli.columns)
+print(comuni)
 '''
-castelli.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\castelli.csv", index=False)
-fortezze.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\forti-fortezze.csv", index=False)
-torri_costiere.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\torri-costiere.csv", index=False)
-
+castelli.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\dataset_csv\\castelli.csv", index=False)
+fortezze.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\dataset_csv\\forti-fortezze.csv", index=False)
+torri_costiere.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\dataset_csv\\torri-costiere.csv", index=False)
+strutture_ricettive.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\dataset_csv\\strutture_ricettive.csv", index=False)
+comuni.to_csv("C:\\Users\\harub\\Documents\\GitHub\\ProgettoOpenData2023\\dataset_finali\\dataset_csv\\comuni.csv", index=False)
